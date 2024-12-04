b4d64c9ca4f84d68b7a97db293b1df835434e0de
Eco-Gaming <7ecogaming4@gmail.com>
2024-12-04 11:21:02 +0100

    day02 - part2

--- src/main/kotlin/puzzles/Day02.kt
+++ src/main/kotlin/puzzles/Day02.kt
@@ -25,18 +25,45 @@ class Day02 : Puzzle {
     override fun solvePartOne(): String {
         var sum = 0
         for (list in matrix) {
-            val diffList = mutableListOf<Int>()
-            for (i in 0..<list.size - 1) {
-                diffList.add(list[i] - list[i + 1])
-            }
-            if ((diffList.all { it > 0 }) xor (diffList.all { it < 0 })) {
-                if (diffList.all { abs(it) in 1..3 }) sum++
-            }
+            val diffList = getDiffList(list)
+            if (checkDiffList(diffList)) sum++
         }
         return sum.toString()
     }
 
     override fun solvePartTwo(): String {
-        TODO("Not yet implemented")
+        var sum = 0
+        for (list in matrix) {
+            val diffList = getDiffList(list)
+            if (checkDiffList(diffList)) {
+                sum++
+            } else {
+                for (i in list.indices) {
+                    val listModified = list.toMutableList()
+                    listModified.removeAt(i)
+                    if (checkDiffList(getDiffList(listModified))) {
+                        sum++
+                        break;
+                    }
+                }
+            }
+
+        }
+        return sum.toString()
+    }
+
+    private fun getDiffList(line: List<Int>) : List<Int> {
+        val diffList = mutableListOf<Int>()
+        for (i in 0..<line.size - 1) {
+            diffList.add(line[i] - line[i + 1])
+        }
+        return diffList
+    }
+
+    private fun checkDiffList(diffList: List<Int>): Boolean {
+        if ((diffList.all { it > 0 }) xor (diffList.all { it < 0 })) {
+            if (diffList.all { abs(it) in 1..3 }) return true
+        }
+        return false
     }
 }
